6b2fb055ec717c486ba3468fb9ed7ac83c18d7e4
PolGuixe <pol.guixe@gmail.com>
2015-01-06 20:16:34 +0100

    added the .js code

--- solomo_test/solomo_test.js
+++ solomo_test/solomo_test.js
@@ -1,23 +1,63 @@
+var Photos = new Meteor.Collection("photos");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+  var selectedMarkerId = new Blaze.ReactiveVar(null);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Deps.autorun(function () {
+    selectedMarkerId.set(Session.get("currentPhoto"));
+  });
+
+  Tracker.autorun(function () {
+    if (Reload.isWaitingForResume()) {
+      alert("Close and reopen this app to get the new version!");
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.map.helpers({
+    markers: Photos.find(),
+    selectedMarkerId: selectedMarkerId
+  });
+
+  var onSuccess = function (imageData) {
+    var latLng = Geolocation.latLng();
+
+    if (! latLng) {
+      return;
+    }
+
+    Photos.insert({
+      image: imageData,
+      createdAt: new Date(),
+      marker: {
+        lat: latLng.lat,
+        lng: latLng.lng,
+        infoWindowContent: "<img width='100' src='" + imageData + "' />"
+      }
+    });
+
+    Router.go("/list");
+  };
+
+  Template.layout.events({
+    "click .photo-link": function () {
+      MeteorCamera.getPicture(function (error, data) {
+        // we have a picture
+        if (! error) {
+          onSuccess(data);
+        }
+      });
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.layout.helpers({
+    onPage: function (pageName) {
+      return Router.current().route.name === pageName;
+    }
+  });
+
+  Template.list.helpers({
+    photos: function () {
+      return Photos.find({}, {sort: {"createdAt": -1}});
+    }
   });
 }
